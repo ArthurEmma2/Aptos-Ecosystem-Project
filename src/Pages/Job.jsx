3c7dcc9b1ad2fcfbf52019cb4dc970df808a1205
Arthur Emmanuel <yhawnoworries@gmail.com>
2022-10-14 23:07:32 +0000

    job section

--- src/Pages/Job.jsx
+++ src/Pages/Job.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 function Job() {
   return (
     <>
-    <section className='container mt-32'>
-      <div>
-        <p className='text-white sm:text-5xl lg:text-7xl font-bold text-center'>Find out more about aptos ecosystem jobs</p>
+    <section className='container mt-52'>
+      <div className='text-center'>
+        <p className='text-white sm:text-5xl lg:text-7xl font-bold text-center'>Aptos! layer 1 for everyone</p>
+        <button className='bg-[#1DE9B6] text-center mx-auto py-2 px-5 rounded-sm mt-8 text-xl hover:opacity-40'>Post a job</button>
         </div>
+
+        
     </section>
     
     </>
