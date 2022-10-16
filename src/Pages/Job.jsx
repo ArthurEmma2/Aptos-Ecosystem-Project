2c4fd8f5be6312d78e24b7a08738f7f80f85403e
Arthur Emmanuel <yhawnoworries@gmail.com>
2022-10-16 22:20:21 +0000

    update

--- src/Pages/Job.jsx
+++ src/Pages/Job.jsx
@@ -6,10 +6,27 @@ function Job() {
     <>
     <section className='container mt-52'>
       <div className='text-center'>
-        <p className='text-white sm:text-5xl lg:text-7xl font-bold text-center'>Aptos! layer 1 for everyone</p>
+        <p className='text-white text-3xl lg:text-7xl font-bold text-center'>Aptos! layer 1 for everyone</p>
         <button className='bg-white shadow-md shadow-slate-400 text-center mx-auto py-2 px-5 rounded-lg mt-8 text-xl hover:opacity-40'>Post a job</button>
         </div>
           <div className='bg-[#1DE9B6] rounded-md shadow-md shadow-slate-600 w-full mt-32 h-32 items-center px-4'>
+            <div className='flex justify-between items-center'>
+              <div className='flex'>
+                <div>
+                <img  className='mt-5' src={apt} width='70px' alt='logo'/>
+                  </div>
+                    <div className='m-7'>
+                    <button className='text-black text-[12px] md:text-2xl  font-bold hover:underline hover:underline-offset-4 hover:transition-all'>Front End Engineer</button>
+                    <p className='font-semibold  text-[10px] md:text-xl'>Aptos Labs</p>
+                    <p className='opacity-80 text-[10px] md:text-xl'>Remote-Global</p>
+                </div>
+              </div>
+              <div className='hidden md:flex'>
+                <button className='text-black'>Apply now<i class="ri-arrow-right-fill"></i></button>
+              </div>
+            </div>
+          </div>
+          <div className='bg-[#1DE9B6] rounded-md shadow-md mt-12 shadow-slate-600 w-full h-32 items-center px-4'>
             <div className='flex justify-between items-center'>
               <div className='flex'>
                 <div>
@@ -22,10 +39,46 @@ function Job() {
                 </div>
               </div>
               <div className='hidden md:flex'>
-                <button className='text-black'>Read more <i class="ri-arrow-right-fill"></i></button>
+                <button className='text-black'>Apply now<i class="ri-arrow-right-fill"></i></button>
               </div>
             </div>
           </div>
+          <div className='bg-[#1DE9B6] rounded-md shadow-md mt-12 shadow-slate-600 w-full h-32 items-center px-4'>
+            <div className='flex justify-between items-center'>
+              <div className='flex'>
+                <div>
+                <img  className='mt-5' src={apt} width='70px' alt='logo'/>
+                  </div>
+                    <div className='m-7'>
+                    <button className='text-black text-[12px] md:text-2xl font-bold hover:underline hover:underline-offset-4 hover:transition-all'>Front End Engineer</button>
+                    <p className='font-semibold  text-[10px] md:text-xl'>Aptos Labs</p>
+                    <p className='opacity-80 text-[10px] md:text-xl'>Remote-Global</p>
+                </div>
+              </div>
+              <div className='hidden md:flex'>
+                <button className='text-black'>Apply now<i class="ri-arrow-right-fill"></i></button>
+              </div>
+            </div>
+          </div>
+          <div className='bg-[#1DE9B6] rounded-md shadow-md mt-12 shadow-slate-600 w-full h-32 items-center px-4'>
+            <div className='flex justify-between items-center'>
+              <div className='flex'>
+                <div>
+                <img  className='mt-5' src={apt} width='70px' alt='logo'/>
+                  </div>
+                    <div className='m-7'>
+                    <button className='text-black text-[12px] md:text-2xl font-bold hover:underline hover:underline-offset-4 hover:transition-all'>Front End Engineer</button>
+                    <p className='font-semibold  text-[10px] md:text-xl'>Aptos Labs</p>
+                    <p className='opacity-80 text-[10px] md:text-xl'>Remote-Global</p>
+                </div>
+              </div>
+              <div className='hidden md:flex'>
+                <button className='text-black'>Apply now<i class="ri-arrow-right-fill"></i></button>
+              </div>
+            </div>
+          </div>
+
+          <p className='mt-52'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum voluptatum laudantium, molestiae, nesciunt beatae expedita error asperiores, nam reprehenderit eum consequuntur tempore esse doloribus suscipit pariatur est animi quibusdam distinctio.</p>
 
     </section>
     
